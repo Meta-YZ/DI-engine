97e6ebde98e22b2b05f81e08eaf6b49f79203c23
Xu Jingxin <sailxjx@gmail.com>
2022-02-23 10:28:08 +0800

    Return self in context methods

--- ding/framework/context.py
+++ ding/framework/context.py
@@ -15,7 +15,7 @@ class Context:
     env_step = 0
     _kept_keys = set(["total_step", "train_iter", "env_step"])
 
-    def setdefault(self, key: str, default: Any) -> None:
+    def setdefault(self, key: str, default: Any) -> "Context":
         """
         Overview:
             Insert key with a value of default if key is not in the dictionary.
@@ -24,12 +24,11 @@ class Context:
             - key (:obj:`str`): Key.
             - default (:obj:`Any`): Default value.
         """
-        if hasattr(self, key):
-            getattr(self, key)
-        else:
+        if not hasattr(self, key):
             setattr(self, key, default)
+        return self
 
-    def renew(self) -> 'Context':  # noqa
+    def renew(self) -> 'Context':
         """
         Overview:
             Renew context from self, add total_step and shift kept properties to the new instance.
@@ -40,10 +39,11 @@ class Context:
                 setattr(ctx, key, getattr(self, key))
         return ctx
 
-    def keep(self, *keys: str) -> None:
+    def keep(self, *keys: str) -> "Context":
         """
         Overview:
             Keep this key/keys until next iteration.
         """
         for key in keys:
             self._kept_keys.add(key)
+        return self
